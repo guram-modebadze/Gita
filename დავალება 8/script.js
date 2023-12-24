d039163e4a3fdc6eaf9008ef9a363fa91d89ffe2
Guram Modebadze <114229679+guram-modebadze@users.noreply.github.com>
2023-12-24 23:35:04 +0400

    Add files via upload

---
+++ დავალება 8/script.js
@@ -0,0 +1,26 @@
+/*-------1-------*/
+
+// let hi = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < hi.length + 1; i++) {
+//   console.log(i);
+// }
+
+/*-------2-------*/
+
+// const salary = prompt("salary");
+// if (salary > 1500) {
+//   alert('მაღალი');
+// } else if (salary > 750) {
+//   alert('საშუალო');
+// } else if(salary<=750) {
+//   alert('წადი სახლიდან');
+// }
+
+/*-------3-------*/
+
+// const nums = [15, 53, 22, 198, 10, 28, 16, 70, 33, 951];
+// nums.forEach(function (k) {
+//   if (k % 2 !==0) {
+//     console.log(k);
+//   }
+// });
